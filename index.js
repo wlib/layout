cd79188ae0715ff583a313679783de1dd443e2af
Daniel Ethridge <dkeout@gmail.com>
2019-04-20 21:14:25 -0400

    Unify Draw and Fill Grid

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 const state = {
-  mode: "Draw Grid", // Draw Grid | Fill Grid | Modify Grid | Modify Item | Final View
-  items: [
-    { id: "header", tag: "header", content: "Header" },
-    { id: "nav", tag: "nav", content: "Navigation" },
-    { id: "hero", tag: "div", content: "Hero" },
-    { id: "main", tag: "main", content: "Main content" },
-    { id: "aside", tag: "aside", content: "Aside content" },
-    { id: "footer", tag: "footer", content: "Footer" }
-  ],
+  items: {
+    header: { tag: "header", content: "Header",        color: `hsl(0, 75%, 70%)`   },
+    nav:    { tag: "nav",    content: "Navigation",    color: `hsl(60, 75%, 70%)`  },
+    hero:   { tag: "div",    content: "Hero",          color: `hsl(120, 75%, 70%)` },
+    main:   { tag: "main",   content: "Main content",  color: `hsl(180, 75%, 70%)` },
+    aside:  { tag: "aside",  content: "Aside content", color: `hsl(240, 75%, 70%)` },
+    footer: { tag: "footer", content: "Footer",        color: `hsl(300, 75%, 70%)` }
+  },
   grid: {
     columns: [
-      "10%", "1fr", "10%"
+      "1fr", "6fr", "2fr"
     ],
     rows: [
       "1fr",
       "80vh",
-      "auto",
+      "minmax(150vh, max-content)",
       "1fr"
     ],
     areas: [
@@ -65,37 +64,16 @@ const format = areas => {
   }))
 }
 
-const renderFillGrid = () => {
-  document.querySelector("#body").innerHTML =
-    state.items
-      .map(item => `<${item.tag} id="${item.id}">${item.content}</${item.tag}>`)
-      .join("\n")
-
-  document.querySelector("style").innerHTML =
-`#body {
-  display: grid;
-  grid-template-columns: ${state.grid.columns.join(" ")};
-  grid-template-rows:
-    ${state.grid.rows.join("\n    ")}
-    ;
-  grid-template-areas:
-    ${format(state.grid.areas)
-        .map(row => '"' + row.join(" ") + '"')
-        .join("\n    ")}
-    ;
-}` + state.items.map((item, i) =>
-`#${item.id} {
-  grid-area: ${item.id};
-  background-color: ${`hsl(${i * 360 / state.items.length}, ${65}%, ${70}%)`};
-}`).join("\n\n")
-}
-
 const renderDrawGrid = () => {
   document.querySelector("#body").innerHTML =
   state.grid.rows.map((row, rowi) =>
-    state.grid.columns.map((column, columni) =>
-      `<div id="${rowi}|${columni}">${rowi}|${columni}</div>`
-    ).join("\n")
+    state.grid.columns.map((column, columni) => {
+      const area = state.grid.areas[rowi][columni]
+      if (area == ".") {
+        return `<div id="${rowi}|${columni}"> <div>[Blank]</div> </div>`
+      }
+      return `<div id="${rowi}|${columni}" style="background-color: ${state.items[area].color};"> <div>${area}</div> </div>`
+    }).join("\n")
   ).join("\n")
 
   document.querySelector("style").innerHTML =
@@ -110,6 +88,20 @@ const renderDrawGrid = () => {
 #body > div {
   border-style: solid;
   border-width: thin;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-rows:
+    1fr
+    1fr
+    1fr
+    ;
+}
+
+#body > div > div {
+  grid-row: 2;
+  grid-column: 2;
+  justify-self: center;
+  align-self: center;
 }`
 }
 
